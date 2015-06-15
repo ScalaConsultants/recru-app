b44877871072ae2999f410888dd80642d4b68a70
Marek Suscak <marek.suscak@gmail.com>
2015-06-15 23:19:14 +0200

    Pull out an initial screen and path selection screen.

--- src/client/pages/notfound.react.js
+++ src/client/pages/notfound.react.js
@@ -2,13 +2,14 @@ import Component from '../components/component.react';
 import DocumentTitle from 'react-document-title';
 import React from 'react';
 import {Link} from 'react-router';
+import './page.styl';
 
 class NotFound extends Component {
 
   render() {
     return (
       <DocumentTitle title="Page Not Found">
-        <div className="notfound-page">
+        <div className="notfound-page page">
           <h1>This page isn't available</h1>
           <p>The link may be broken, or the page may have been removed.</p>
           <Link to="home">Continue here please.</Link>
---
+++ src/client/screens/pathselection.react.js
@@ -0,0 +1,31 @@
+import Component from '../components/component.react';
+import React from 'react';
+
+export default class PathSelection extends Component {
+  render() {
+    return (
+      <section className="path-selection-screen">
+        <header>Choose your path</header>
+        <ul>
+          <li>
+            <span><em>Backend</em> Climber</span>
+            <img alt="Backend Climber path" src="" />
+          </li>
+          <li>
+            <span><em>Frontend</em> Explorer</span>
+            <img alt="Frontend Explorer path" src="" />
+          </li>
+          <li>
+            <span><em>Mobile</em> Hiker</span>
+            <img alt="Mobile Hiker path" src="" />
+          </li>
+          <li>
+            <span><em>devOps</em> Sherpa</span>
+            <img alt="devOps Sherpa path" src="" />
+          </li>
+        </ul>
+      </section>
+    );
+  }
+
+}
