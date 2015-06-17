6e697a3e9f95328cae4e977c57483a8e26339d93
Marek Suscak <marek.suscak@gmail.com>
2015-06-17 17:27:04 +0200

    Allow the transition from third to fourth screen.

--- src/client/screens/third-screen.react.js
+++ src/client/screens/third-screen.react.js
@@ -1,5 +1,6 @@
 import Component from '../components/component.react';
 import React from 'react';
+import * as actions from './actions';
 import './third-screen.styl';
 
 export default class ThirdScreen extends Component {
@@ -7,19 +8,19 @@ export default class ThirdScreen extends Component {
     return (
       <section className="third-screen screen">
         <ul>
-          <li>
+          <li onClick={actions.nextScreen}>
             <p><strong>Backend</strong> Climber</p>
             <img alt="Backend Climber path" src="assets/img/climber.JPG" />
           </li>
-          <li>
+          <li onClick={actions.nextScreen}>
             <p><strong>Frontend</strong> Explorer</p>
             <img alt="Frontend Explorer path" src="assets/img/climber.JPG" />
           </li>
-          <li>
+          <li onClick={actions.nextScreen}>
             <p><strong>Mobile</strong> Hiker</p>
             <img alt="Mobile Hiker path" src="assets/img/climber.JPG" />
           </li>
-          <li>
+          <li onClick={actions.nextScreen}>
             <p><strong>devOps</strong> Sherpa</p>
             <img alt="devOps Sherpa path" src="assets/img/climber.JPG" />
           </li>
