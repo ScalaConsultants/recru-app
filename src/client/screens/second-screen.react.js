fd0605fc712aec8a1b6b18f87975e445290ce772
Marek Suscak <marek.suscak@gmail.com>
2015-06-17 17:15:55 +0200

    Bind the nextScreen action to chevron's onClick event.

--- src/client/screens/second-screen.react.js
+++ src/client/screens/second-screen.react.js
@@ -1,5 +1,7 @@
 import Component from '../components/component.react';
+import Chevron from '../components/chevron.react';
 import React from 'react';
+import * as actions from './actions';
 import './second-screen.styl';
 
 export default class SecondScreen extends Component {
@@ -12,10 +14,10 @@ export default class SecondScreen extends Component {
             <span>There are</span>
             <span><strong>several ways</strong></span>
             <span>to the <strong>top</strong></span>
-            <button tabIndex="-1">choose your path</button>
+            <button onClick={actions.nextScreen} tabIndex="-1">choose your path</button>
+            <Chevron isAnimated={true} onClick={actions.nextScreen}/>
           </h1>
         </div>
-        
       </section>
     );
   }
