7073fca6745855dd0e5eb2cd703c2348060bed63
Marek Suscak <marek.suscak@gmail.com>
2015-06-18 15:57:32 +0200

    Handle edge cases gracefully.

--- src/client/screens/store.js
+++ src/client/screens/store.js
@@ -2,20 +2,26 @@ import * as actions from './actions';
 import {register} from '../dispatcher';
 import {screensCursor} from '../state';
 
+const NO_OF_SCREENS = 5;
+
 export const dispatchToken = register(({action, data}) => {
 
   switch (action) {
     case actions.nextScreen:
       screensCursor(screens => {
-        const previousScreen = screens.get('currentScreen');
-        return screens.set('currentScreen', previousScreen + 1);
+        const nextScreen = screens.get('currentScreen') + 1;
+        if (nextScreen >= NO_OF_SCREENS)
+          return screens.get('currentScreen');
+        return screens.set('currentScreen', nextScreen);
       });
       break;
 
     case actions.previousScreen:
       screensCursor(screens => {
-        const previousScreen = screens.get('currentScreen');
-        return screens.set('currentScreen', previousScreen - 1);
+        const previousScreen = screens.get('currentScreen') - 1;
+        if (previousScreen < 0)
+          return screens.get('currentScreen');
+        return screens.set('currentScreen', previousScreen);
       });
       break;
   }
