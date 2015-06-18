79b7b1012fbcd0792a30d6655732cd2cde375292
Marek Suscak <marek.suscak@gmail.com>
2015-06-18 16:39:48 +0200

    Add dummy minimap.

---
+++ src/client/components/minimap.react.js
@@ -0,0 +1,13 @@
+import Component from '../components/component.react';
+import React from 'react';
+import './minimap.styl';
+
+class MiniMap extends Component {
+  render() {
+    return (
+      <div className="minimap"></div>
+    );
+  }
+}
+
+export default MiniMap;
