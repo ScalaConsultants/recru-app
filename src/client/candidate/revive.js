7e6604966d6fa55d722f4e418d6b7fa41154433f
Marek Suscak <marek.suscak@gmail.com>
2015-06-18 22:00:55 +0200

    Implement revivers properly.

--- src/client/candidate/revive.js
+++ src/client/candidate/revive.js
@@ -2,8 +2,8 @@ import {Map} from 'immutable';
 
 export default function(value) {
   return Map(value)
-    .set('name', '')
-    .set('role', null)
-    .set('skills', [])
-    .set('resume', null);
+    .set('name', value.get('name'))
+    .set('role', value.get('role'))
+    .set('skills', value.get('skills'))
+    .set('resume', value.get('resume'));
 }
--- src/client/screens/revive.js
+++ src/client/screens/revive.js
@@ -2,6 +2,6 @@ import {Map} from 'immutable';
 
 export default function(value) {
   return Map(value)
-    .set('currentScreen', 0)
-    .set('lastScreen', 4);
+    .set('currentScreen', value.get('currentScreen'))
+    .set('lastScreen', value.get('lastScreen'));
 }
