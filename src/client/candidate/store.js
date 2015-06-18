57e2c731ab4cb489dbb81ba8d4580222e765aa2d
Marek Suscak <marek.suscak@gmail.com>
2015-06-18 20:41:38 +0200

    Implement candidate store.

--- src/client/candidate/store.js
+++ src/client/candidate/store.js
@@ -6,15 +6,27 @@ export const dispatchToken = register(({action, data}) => {
 
   switch (action) {
     case actions.saveName:
+      candidateCursor(candidate => {
+        return candidate.set('name', data);
+      });
       break;
 
     case actions.saveRole:
+      candidateCursor(candidate => {
+        return candidate.set('role', data);
+      });
       break;
 
     case actions.saveSkill:
+      candidateCursor(candidate => {
+        return candidate.set(['skills', data.title], data.rating);
+      });
       break;
 
     case actions.saveResume:
+      candidateCursor(candidate => {
+        return candidate.set('resume', data);
+      });
       break;
   }
 });
