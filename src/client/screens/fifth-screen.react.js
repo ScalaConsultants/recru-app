8bf43f39e08fc4501af976d293c3c985e4b6b464
kajas90 <katarzyna.jastrzebska90@gmail.com>
2015-06-18 17:13:19 +0200

    fifth screen styling

--- src/client/screens/fifth-screen.react.js
+++ src/client/screens/fifth-screen.react.js
@@ -1,11 +1,15 @@
 import Component from '../components/component.react';
 import React from 'react';
+import './fifth-screen.styl';
 
 export default class FifthScreen extends Component {
   render() {
     return (
       <section className="fifth-screen screen">
         <header>One more thing...</header>
+        <div class="drop-area" id="drop">
+          <span>drop your resume</span>
+        </div>
       </section>
     );
   }
