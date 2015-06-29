d263e2714d8d8db82b595ca581d08efaaa36c565
Marek Suscak <marek.suscak@gmail.com>
2015-06-29 18:14:45 +0200

    Add touch events handling. Fixes #27.

--- src/client/mixins/bound-scroll.js
+++ src/client/mixins/bound-scroll.js
@@ -2,25 +2,64 @@ import React from 'react';
 
 // TODO: shall be refactored into a higher order component
 export default {
-  handleMouseWheel: function(e) {
+  swipeDetails: {
+    sY: 0,
+    eY: 0,
+    sScrollTop: 0
+  },
+
+  isWithinBounds: function() {
     // Stop wheel event propagation if not at the topmost or bottommost part
     const element = React.findDOMNode(this);
     const maxScrollPosition = element.scrollHeight - element.clientHeight;
-    if (element.scrollTop > 0 && element.scrollTop < maxScrollPosition)
+    return (element.scrollTop > 0 && element.scrollTop < maxScrollPosition);
+  },
+
+  hasStartedAtBounds: function() {
+    const element = React.findDOMNode(this);
+    const maxScrollPosition = element.scrollHeight - element.clientHeight;
+    return (this.swipeDetails.sScrollTop === 0 || this.swipeDetails.sScrollTop === maxScrollPosition);
+  },
+
+  handleMouseWheel: function(e) {
+    if (this.isWithinBounds())
+      e.stopPropagation();
+  },
+
+  handleTouchStart: function(e) {
+    this.swipeDetails.sY = e.touches[0].screenY;
+    this.swipeDetails.sScrollTop = React.findDOMNode(this).scrollTop;
+  },
+
+  handleTouchMove: function(e) {
+    this.swipeDetails.eY = e.touches[0].screenY;
+  },
+
+  handleTouchEnd: function(e) {
+    if (this.isWithinBounds() || !this.hasStartedAtBounds())
       e.stopPropagation();
   },
 
   componentWillMount: function() {
+    this.handleTouchStart = this.handleTouchStart.bind(this);
+    this.handleTouchMove = this.handleTouchMove.bind(this);
+    this.handleTouchEnd = this.handleTouchEnd.bind(this);
     this.handleMouseWheel = this.handleMouseWheel.bind(this);
   },
 
   componentDidMount: function() {
     React.findDOMNode(this).addEventListener('mousewheel', this.handleMouseWheel, false);
     React.findDOMNode(this).addEventListener('DOMMouseScroll', this.handleMouseWheel, false);
+    React.findDOMNode(this).addEventListener('touchstart', this.handleTouchStart, false);
+    React.findDOMNode(this).addEventListener('touchmove', this.handleTouchMove, false);
+    React.findDOMNode(this).addEventListener('touchend', this.handleTouchEnd, false);
   },
 
   componentWillUnmount: function() {
     React.findDOMNode(this).removeEventListener('mousewheel', this.handleMouseWheel);
     React.findDOMNode(this).removeEventListener('DOMMouseScroll', this.handleMouseWheel);
+    React.findDOMNode(this).removeEventListener('touchstart', this.handleTouchStart);
+    React.findDOMNode(this).removeEventListener('touchmove', this.handleTouchMove);
+    React.findDOMNode(this).removeEventListener('touchend', this.handleTouchEnd);
   }
 };
--- src/client/mixins/movement-handler.js
+++ src/client/mixins/movement-handler.js
@@ -2,6 +2,11 @@ import throttle from 'lodash.throttle';
 
 // TODO: shall be refactored into a higher order component
 export default {
+  swipeDetails: {
+    sY: 0,
+    eY: 0
+  },
+
   handleMouseWheel: function(e) {
     const delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
     if (delta > 0)
@@ -10,7 +15,26 @@ export default {
       this.handleMoveDown();
   },
 
+  handleTouchStart: function(e) {
+    this.swipeDetails.sY = e.touches[0].screenY;
+  },
+
+  handleTouchMove: function(e) {
+    this.swipeDetails.eY = e.touches[0].screenY;
+  },
+
+  handleTouchEnd: function(e) {
+    const delta = this.swipeDetails.eY - this.swipeDetails.sY;
+    if (delta > 0)
+      this.handleMoveUp();
+    else if (delta < 0)
+      this.handleMoveDown();
+  },
+
   componentWillMount: function() {
+    this.handleTouchStart = this.handleTouchStart.bind(this);
+    this.handleTouchMove = this.handleTouchMove.bind(this);
+    this.handleTouchEnd = this.handleTouchEnd.bind(this);
     this.handleMouseWheel = this.handleMouseWheel.bind(this);
     this.handleMouseWheel = throttle(this.handleMouseWheel, 1000, {
       'leading': true,
@@ -21,10 +45,16 @@ export default {
   componentDidMount: function() {
     window.addEventListener('mousewheel', this.handleMouseWheel, false);
     window.addEventListener('DOMMouseScroll', this.handleMouseWheel, false);
+    window.addEventListener('touchstart', this.handleTouchStart, false);
+    window.addEventListener('touchmove', this.handleTouchMove, false);
+    window.addEventListener('touchend', this.handleTouchEnd, false);
   },
 
   componentWillUnmount: function() {
     window.removeEventListener('mousewheel', this.handleMouseWheel);
     window.removeEventListener('DOMMouseScroll', this.handleMouseWheel);
+    window.removeEventListener('touchstart', this.handleTouchStart);
+    window.removeEventListener('touchmove', this.handleTouchMove);
+    window.removeEventListener('touchend', this.handleTouchEnd);
   }
 };
