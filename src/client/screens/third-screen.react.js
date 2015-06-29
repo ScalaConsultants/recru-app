1b57c74dde24219b89c38514d4d704c102ca5884
Marek Suscak <marek.suscak@gmail.com>
2015-06-29 14:33:22 +0200

    Load roles from the json file.

--- src/client/screens/third-screen.react.js
+++ src/client/screens/third-screen.react.js
@@ -2,6 +2,7 @@ import Component from '../components/component.react';
 import React from 'react';
 import boundScrollMixin from '../mixins/bound-scroll';
 import reactMixin from 'react-mixin';
+import json from '../data/roles.json';
 import {saveRole} from '../candidate/actions';
 import {nextScreen} from './actions';
 import './third-screen.styl';
@@ -21,34 +22,17 @@ class ThirdScreen extends Component {
     return (
       <section className="third-screen screen">
         <ul>
-          <li onClick={() => this.proceed(0)}>
-            <p><strong>Backend</strong> Climber</p>
-            <div>
-              <img alt="Backend Climber path" src="assets/img/climber.jpg" />
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sagittis mi ipsum, at rutrum tellus egestas eget. Curabitur dolor sem, tincidunt pulvinar vestibulum vel, luctus hendrerit velit. Donec lobortis varius facilisis.</p>
-            </div>
-          </li>
-          <li onClick={() => this.proceed(1)}>
-            <p><strong>Clojure</strong> Explorer</p>
-            <div>
-              <img alt="Clojure Explorer path" src="assets/img/explorer.jpg" />
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sagittis mi ipsum, at rutrum tellus egestas eget. Curabitur dolor sem, tincidunt pulvinar vestibulum vel, luctus hendrerit velit. Donec lobortis varius facilisis.</p>
-            </div>
-          </li>
-          <li onClick={() => this.proceed(2)}>
-            <p><strong>Mobile</strong> Hiker</p>
-            <div>
-              <img alt="Mobile Hiker path" src="assets/img/hiker.jpg" />
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sagittis mi ipsum, at rutrum tellus egestas eget. Curabitur dolor sem, tincidunt pulvinar vestibulum vel, luctus hendrerit velit. Donec lobortis varius facilisis.</p>
-            </div>
-          </li>
-          <li onClick={() => this.proceed(3)}>
-            <p><strong>Frontend</strong> Observer</p>
-            <div>
-              <img alt="frontend observer path" src="assets/img/observer.jpg" />
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sagittis mi ipsum, at rutrum tellus egestas eget. Curabitur dolor sem, tincidunt pulvinar vestibulum vel, luctus hendrerit velit. Donec lobortis varius facilisis.</p>
-            </div>
-          </li>
+          {json.map((role) => {
+            return (
+              <li key={role.id} onClick={() => this.proceed(role.id)}>
+                <p><strong>{role.name.split(' ')[0]}</strong> {role.name.split(' ')[1]}</p>
+                <div>
+                  <img alt={`${role.name} path`} src={role.img} />
+                  <p>{role.description}</p>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </section>
     );
