4447237eeba993021b2dbd6673ed73a74ccfba39
Marek Suscak <marek.suscak@gmail.com>
2015-06-29 14:17:45 +0200

    Remove unused utility function.

--- src/test/client/lib/getrandomstring.spec.js
+++
@@ -1,8 +0,0 @@
-import {expect} from 'chai';
-import {getRandomString} from 'client/lib/getrandomstring';
-
-describe('getRandomString', () => {
-  it('is a string', () => {
-    expect(getRandomString()).to.be.a('string');
-  });
-});
