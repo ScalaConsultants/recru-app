18679b69a7ad22589fcda4feccee57c8bff052ec
Joanna <asia@Joannas-MBP.lan>
2016-10-20 15:01:28 +0200

    Change foot animation. Change a sign img on Pack Your Bag Screen.

--- src/browser/components/SignpostImg.react.js
+++ src/browser/components/SignpostImg.react.js
@@ -71,23 +71,41 @@ export default class SignpostImg extends Component {
   pushItems() {
     this.setState({
       feet: [
-        {'bottom': 80, 'left': 27, 'rotation': -200, 'delay': 0.0, 'scale': 0.74},
-        {'bottom': 75, 'left': 23, 'rotation': -190, 'delay': 0.5, 'scale': 0.74},
+        {'bottom': 62, 'left': 27, 'rotation': -300, 'delay': 0.0, 'scale': 0.35},
+        {'bottom': 57, 'left': 26, 'rotation': -300, 'delay': 0.5, 'scale': 0.35},
 
-        {'bottom': 71, 'left': 30, 'rotation': -200, 'delay': 1.0, 'scale': 0.78},
-        {'bottom': 64, 'left': 26, 'rotation': -200, 'delay': 1.5, 'scale': 0.78},
+        {'bottom': 64, 'left': 35, 'rotation': -270, 'delay': 1.0, 'scale': 0.45},
+        {'bottom': 59, 'left': 33, 'rotation': -270, 'delay': 1.5, 'scale': 0.45},
 
-        {'bottom': 61, 'left': 34, 'rotation': -220, 'delay': 2.0, 'scale': 0.82},
-        {'bottom': 53, 'left': 32, 'rotation': -230, 'delay': 2.5, 'scale': 0.82},
+        {'bottom': 64, 'left': 45, 'rotation': -270, 'delay': 2.0, 'scale': 0.55},
+        {'bottom': 59, 'left': 43, 'rotation': -270, 'delay': 2.5, 'scale': 0.55},
 
-        {'bottom': 54, 'left': 41, 'rotation': -250, 'delay': 3.0, 'scale': 0.86},
-        {'bottom': 46, 'left': 44, 'rotation': -260, 'delay': 3.5, 'scale': 0.86},
+        {'bottom': 65, 'left': 55, 'rotation': -290, 'delay': 3.0, 'scale': 0.65},
+        {'bottom': 59, 'left': 53, 'rotation': -280, 'delay': 3.5, 'scale': 0.65},
 
-        {'bottom': 52, 'left': 51, 'rotation': -280, 'delay': 4.0, 'scale': 0.90},
-        {'bottom': 47, 'left': 57, 'rotation': -290, 'delay': 4.5, 'scale': 0.90},
+        {'bottom': 69, 'left': 63, 'rotation': -310, 'delay': 4.0, 'scale': 0.75},
+        {'bottom': 63, 'left': 64, 'rotation': -310, 'delay': 4.5, 'scale': 0.75}
 
-        {'bottom': 57, 'left': 60, 'rotation': -320, 'delay': 5.0, 'scale': 0.95},
-        {'bottom': 55, 'left': 67, 'rotation': -310, 'delay': 5.5, 'scale': 0.95}
+
+
+
+        // {'bottom': 80, 'left': 27, 'rotation': -200, 'delay': 0.0, 'scale': 0.74},
+        // {'bottom': 75, 'left': 23, 'rotation': -190, 'delay': 0.5, 'scale': 0.74},
+        //
+        // {'bottom': 71, 'left': 30, 'rotation': -200, 'delay': 1.0, 'scale': 0.78},
+        // {'bottom': 64, 'left': 26, 'rotation': -200, 'delay': 1.5, 'scale': 0.78},
+        //
+        // {'bottom': 61, 'left': 34, 'rotation': -220, 'delay': 2.0, 'scale': 0.82},
+        // {'bottom': 53, 'left': 32, 'rotation': -230, 'delay': 2.5, 'scale': 0.82},
+        //
+        // {'bottom': 54, 'left': 41, 'rotation': -250, 'delay': 3.0, 'scale': 0.86},
+        // {'bottom': 46, 'left': 44, 'rotation': -260, 'delay': 3.5, 'scale': 0.86},
+        //
+        // {'bottom': 52, 'left': 51, 'rotation': -280, 'delay': 4.0, 'scale': 0.90},
+        // {'bottom': 47, 'left': 57, 'rotation': -290, 'delay': 4.5, 'scale': 0.90},
+        //
+        // {'bottom': 57, 'left': 60, 'rotation': -320, 'delay': 5.0, 'scale': 0.95},
+        // {'bottom': 55, 'left': 67, 'rotation': -310, 'delay': 5.5, 'scale': 0.95}
       ]
     });
   }
@@ -96,7 +114,7 @@ export default class SignpostImg extends Component {
     return (
       <div className="sign-img">
         <div>
-          <img alt='Signpist to Scalac img' id='img1' onClick={e => this.proceed(e)} src='../../../assets/img/packYourBag/recruapp_znak_1.png' />
+          <img alt='Signpist to Scalac img' id='img1' onClick={e => this.proceed(e)} src='../../../assets/img/packYourBag/gory1.svg' />
           {this.state.feet.map((foot, idx) => {
             const footSide = (idx % 2) ? 'right' : 'left';
             const imageSrc = '../../../assets/img/packYourBag/' + footSide + '_footprint.svg';
