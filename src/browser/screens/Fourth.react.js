50af363c307ed5d2c671717d3345421020c79d9d
Joanna <asia@Joannas-MBP.lan>
2016-12-21 09:29:24 +0100

    Style changes.

--- src/browser/screens/Fourth.react.js
+++ src/browser/screens/Fourth.react.js
@@ -65,16 +65,10 @@ export default class FourthScreen extends Component {
 
     return (
       <section className="fourth-screen screen">
-        <div className="screen-title">
-          <h1>
-            <strong>Show us Your skills</strong>
-          </h1>
-        </div>
-
-        <div className="screen-img">
-          <img alt="Geek" src="../../../assets/img/skills/geek.svg"/>
-        </div>
-        <ul className="skills-list">
+        <header>
+          Show us Your skills
+        </header>
+        <ul>
           {skillsForCurrentRole.map((skill) =>
             <SkillItem actions={this.props.actions} data={skill} key={skill.id} resetErrorStatus={this.resetErrorStatus.bind(this)}/>
           )}
