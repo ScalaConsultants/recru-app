94377cb37fd13c41c3cd9238d54e565d6fc17ecc
Alwox <alanwolejko@gmail.com>
2017-04-13 13:55:16 +0200

    change labels to self-closing tags

--- src/browser/components/SkillItem.react.js
+++ src/browser/components/SkillItem.react.js
@@ -41,19 +41,19 @@ export default class SkillItem extends Component {
         <img {...cb} alt={this.props.data.name} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} src={this.props.data.src}/>
         <form action="">
           <input className="star star-5" id={`star5_${this.props.data.id}`} name="star" onChange={(e) => this.handleChange(5, e)} type="radio"/>
-          <label className="star star-5" htmlFor={`star5_${this.props.data.id}`}></label>
+          <label className="star star-5" htmlFor={`star5_${this.props.data.id}`}/>
 
           <input className="star star-4" id={`star4_${this.props.data.id}`} name="star" onChange={(e) => this.handleChange(4, e)} type="radio"/>
-          <label className="star star-4" htmlFor={`star4_${this.props.data.id}`}></label>
+          <label className="star star-4" htmlFor={`star4_${this.props.data.id}`}/>
 
           <input className="star star-3" id={`star3_${this.props.data.id}`} name="star" onChange={(e) => this.handleChange(3, e)} type="radio"/>
-          <label className="star star-3" htmlFor={`star3_${this.props.data.id}`}></label>
+          <label className="star star-3" htmlFor={`star3_${this.props.data.id}`}/>
 
           <input className="star star-2" id={`star2_${this.props.data.id}`} name="star" onChange={(e) => this.handleChange(2, e)} type="radio"/>
-          <label className="star star-2" htmlFor={`star2_${this.props.data.id}`}></label>
+          <label className="star star-2" htmlFor={`star2_${this.props.data.id}`}/>
 
           <input className="star star-1" id={`star1_${this.props.data.id}`} name="star" onChange={(e) => this.handleChange(1, e)} type="radio"/>
-          <label className="star star-1" htmlFor={`star1_${this.props.data.id}`}></label>
+          <label className="star star-1" htmlFor={`star1_${this.props.data.id}`}/>
         </form>
       </li>
     );
