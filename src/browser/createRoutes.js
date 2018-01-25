97bfa4aaa988195598e5beebcddf43305e734ce5
kw_pl <karol.wojdyla@gmail.com>
2018-01-25 17:18:14 +0100

    Update to new version

--- src/browser/createRoutes.js
+++ src/browser/createRoutes.js
@@ -1,16 +1,14 @@
-import App from './app/App.react';
-import React from 'react';
-import Screens from './screens/Page.react';
-import NotFound from './notfound/Page.react';
-import {IndexRoute, Route} from 'react-router';
+import App from './app/App.react'
+import React from 'react'
+import Screens from './screens/Page.react'
+import {IndexRoute, Route} from 'react-router'
 
-export default function createRoutes(getState) {
-
+export default function createRoutes (getState) {
   return (
-    <Route component={App} path="/">
+    <Route component={App} path='/'>
       <IndexRoute component={Screens} />
-      <Route component={NotFound} path="*" />
+      <Route component={Screens} path='*' />
     </Route>
-  );
+  )
 
 }
